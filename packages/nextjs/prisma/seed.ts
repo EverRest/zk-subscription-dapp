fbd960c36a037f0e110aed87832249175bf6c634
pavlo.medynskyi <pavlo@wiz-team.com>
2025-03-05 20:12:04 +0100

    Added seeding categories, subscriptions

--- packages/nextjs/prisma/seed.ts
+++ packages/nextjs/prisma/seed.ts
@@ -5,25 +5,55 @@ import * as path from "path";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  try {
-    const dataPath = path.join(__dirname, "seed-data.json");
-    const rawData = await fs.readFile(dataPath, "utf-8");
-    const seedData = JSON.parse(rawData);
-    const subscriptions = getAllSubscriptionSchemas();
-
-    console.log("Subscriptions seeded:", subscriptions);
-
-    const categories = await prisma.category.createMany({
-      data: seedData.categories,
+async function seedCategories() {
+  const dataPath = path.join(__dirname, "seed-data.json");
+  const rawData = await fs.readFile(dataPath, "utf-8");
+  const seedData = JSON.parse(rawData);
+  console.log(seedData);
+  console.log("🌱 Seeding Categories...");
+  for (const category of seedData.categories) {
+    await prisma.category.upsert({
+      where: { id: category.id },
+      update: category,
+      create: category,
     });
-
-    console.log("Categories seeded:", categories);
-  } catch (error) {
-    console.error("Error seeding the database:", error);
-  } finally {
-    await prisma.$disconnect();
   }
+
+  console.log("✅ Categories seeded successfully!");
 }
 
-main();
+async function seedSubscriptions() {
+  const subscriptions = getAllSubscriptionSchemas();
+  console.log("🌱 Seeding Subscriptions...");
+  for (const subscription of subscriptions) {
+    await prisma.subscription.upsert({
+      where: { id: subscription.id, code: subscription.code },
+      update: {
+        id: subscription.id,
+        code: subscription.code,
+        name: subscription.name,
+      },
+      create: {
+        id: subscription.id,
+        code: subscription.code,
+        name: subscription.name,
+      },
+    });
+  }
+
+  console.log("✅ Subscriptions seeded successfully!");
+}
+
+async function main() {
+  await seedCategories();
+  await seedSubscriptions();
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
